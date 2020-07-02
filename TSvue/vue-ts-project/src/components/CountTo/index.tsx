1d78b5b6371e109ba29fbfbb3f023ddc3c7b2fdb
lorry0508 <1165354824@qq.com>
2020-07-02 18:19:26 +0800

    2020-07-02

--- TSvue/vue-ts-project/src/components/CountTo/index.tsx
+++ TSvue/vue-ts-project/src/components/CountTo/index.tsx
@@ -7,20 +7,27 @@ import CountUp from 'countup'
 
 export default class CountTo extends Vue {
     @Prop({ type: Number, default: 0 }) public readonly start!: number
-    // @Prop(Number) public readonly
+    @Prop(Number) public readonly end!: number
     public counter: CountUp = null;
     public get eleId() { // 计算属性的写法
         return `count_to_${(this as any)._uid}`;
     }
+    public update(endVal: number): void {
+        this.counter.update(endVal);
+    }
+    @Emit('on-click') // 这里的on-click即为自定义事件名
+    public click(event) { // 这个方法名用于组件内调用
+        return event; // return的值即为事件回调函数的参数
+    }
     protected render() {
         return (
-            <div class='count-up-wrap'>
-                <span id={this.eleId}></span>
+            <div class='count-up-wrap' on-click={ this.click }>
+                <span id={ this.eleId }></span>
             </div>
         )
     }
     protected mounted() {
-        this.counter = new CountUp(this.eleId, 0, 1000, 0, 1, {}); // 创建CountUp实例，并保存在counter上
+        this.counter = new CountUp(this.eleId, this.start, this.end, 0, 1, {}); // 创建CountUp实例，并保存在counter上
         this.counter.start(); // 调用此方法让动画效果开始
     }
 }
--- TSvue/vue-ts-project/src/views/Home.vue
+++ TSvue/vue-ts-project/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="home">
-        <count-to></count-to>
-        <count-to></count-to>
+        <count-to @on-click="handleClick" ref="counter" :end="99999"></count-to>
 	</div>
 </template>
 
@@ -16,5 +15,14 @@ import CountTo from '@/components/CountTo';
     }
 })
 
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    public handleClick(event) {
+        console.log(event);
+    }
+    protected mounted() {
+        setInterval(() => {
+            (this.$refs.counter as CountTo).update(Math.random() * 10000); // 这样每2秒钟这里会更新一次，更新的值是随机的
+        }, 2000)
+    }
+}
 </script>
